163f55703545fbb5903d5f7569e36fb64d96aae1
farishuric <faris.huric@multibank.io>
2023-09-10 11:31:47 +0200

    Initial Commit

---
+++ Rolla/RollaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollaApp.swift
+//  Rolla
+//
+//  Created by Faris Hurić on 10. 9. 2023..
+//
+
+import SwiftUI
+
+@main
+struct RollaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
